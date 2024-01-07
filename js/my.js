25db63eefe1c26b04b68508cd8484b37f5ac013e
AnyaK08 <kozina.anya08@gmail.com>
2024-01-07 13:14:24 +0100

    +show all verbs

--- js/my.js
+++ js/my.js
@@ -1,6 +1,50 @@
 let lng;
 
 $(".js-lng").on("click", function () {
-    lng = $(this).attr("id");
-    console.log(lng);
+  lng = $(this).attr("id");
+  //   console.log(lng);
+  getAllVerbs(lng);
 });
+
+function getAllVerbs(lng) {
+  // Инициализация строки HTML
+  var htmlString = "<div class='row'>";
+  let verbs;
+  if (lng === "en") {
+    verbs = en;
+  } else {
+    verbs = ro;
+  }
+  // Проход по массиву объектов
+  for (var i = 0; i < verbs.length; i++) {
+    // Получение значения параметра verb для текущего объекта
+    var verbTranslation = verbs[i].verb;
+
+    // Добавление элемента списка с текущим значением verb в строку HTML
+    htmlString += "<div class='col-2'>" + verbTranslation + "</div>";
+  }
+
+  // Завершение строки HTML
+  htmlString += "</div>";
+
+  $("#verbs").html(htmlString);
+}
+
+var en = [
+  {
+    verb: "play - играть",
+  },
+  {
+    verb: "swim - плавать",
+  },
+ 
+];
+
+var ro = [
+  {
+    verb: "joaca - играть",
+  },
+  {
+    verb: "înot - плавать",
+  },
+];
